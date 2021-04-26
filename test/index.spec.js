96116ff076a1532ff5a6580a39c34dd788f764c0
Erika <erikagonza2013@gmail.com>
2021-04-26 11:47:31 -0500

    test para las funciones para validar

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,19 @@
 // importamos la funcion que vamos a testear
-import { myFunction } from '../src/lib/index';
+import validation from '../src/js/validation.js';
 
-describe('myFunction', () => {
-  it('debería ser una función', () => {
-    expect(typeof myFunction).toBe('function');
+describe('validateUser', () => {
+  it('email correct', () => {
+    expect(validation.validateUser('erika.andreina.3@gmail.com')).toBe(true);
+  });
+  it('email fail', () => {
+    expect(validation.validateUser('erika5501@')).toBe(false);
+  });
+});
+describe('validatePassword', () => {
+  it('password correct', () => {
+    expect(validation.validatePassword('erika987654321')).toBe(true);
+  });
+  it('password fail', () => {
+    expect(validation.validatePassword('1234')).toBe(false);
   });
 });
