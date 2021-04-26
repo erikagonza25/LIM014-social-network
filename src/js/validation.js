8b8c9bf221dc605c2c80ebd66777f6e53111657b
Erika <erikagonza2013@gmail.com>
2021-04-26 18:14:52 -0500

    FMejorando funciones para validar

--- src/js/validation.js
+++ src/js/validation.js
@@ -1,4 +1,42 @@
 // Función que valida el correo
+export const validateEmail = (email) => {
+  // expresión regular que simula el patron de búsqueda del correo electrónico
+  const regEx = /^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/;
+  return regEx.test(email);
+};
+// funcion que valida si los datos de usuario ne estan vacios y email es valido
+
+export const validateUser = (userEmail, userPass) => {
+  if (
+    !validateEmail(userEmail)
+    || userEmail === ''
+    || userPass === ''
+    || userPass < 6
+  ) {
+    return false;
+  }
+  return true;
+};
+// valida datos de nuevo usuario
+export const validateNewUser = (
+  newUserEmail,
+  newUserPass,
+  newUserName,
+  newUserLastName,
+) => {
+  if (
+    !validateEmail(newUserEmail)
+    || newUserEmail === ''
+    || newUserPass === ''
+    || newUserPass < 6
+    || newUserName === ''
+    || newUserLastName === ''
+  ) {
+    return false;
+  }
+  return true;
+};
+/* // Función que valida el correo
 function validateEmail(email) {
   const regEx = /^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/;
   return regEx.test(email);
@@ -21,9 +59,26 @@ const validatePassword = (passwordLogin) => {
   }
   return true;
 };
+// Función para validar el input name
+const validateName = (nicknameCheckin) => {
+  if (nicknameCheckin === '' || nicknameCheckin.length === 0) {
+    return false;
+  }
+  return true;
+};
 
+// Función para validar el input passwordRepeat
+const validatePasswordRepeat = (passwordRepeat, passwordCheckin) => {
+  if (passwordRepeat !== passwordCheckin) {
+    return false;
+  }
+  return true;
+};
 export default {
+  validatePasswordRepeat,
+  validateName,
   validateEmail,
   validateUser,
   validatePassword,
 };
+*/
