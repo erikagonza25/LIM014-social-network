f0ef69ebd64dcbd02a3170df48b96fedc744bee7
kelmita22 <kelmita22@gmail.com>
2021-05-06 17:05:41 -0500

    actualización test

--- test/firestore.spec.js
+++ test/firestore.spec.js
@@ -8,15 +8,9 @@ import {
   getPost,
   commentAdd,
   getComment,
-  removePost,
-  removeComment,
-  profilePhoto,
-  coveragePhoto,
   upgradePost,
-  upgradeComment,
-  upgradeLike,
 } from '../src/js/firestore';
-
+// Collection Firebase mock
 const fixtureData = {
   __collection__: {
     SN_Users: {
@@ -35,7 +29,7 @@ const fixtureData = {
         id_001: {
           date: '',
           likes: '',
-          publication: 'Primera publicación',
+          publication: 'Publicaciones',
           urlimg: '',
           userId: '001',
 
@@ -44,158 +38,98 @@ const fixtureData = {
     },
   },
 };
-
 global.firebase = new MockFirebase(fixtureData, { isNaiveSnapshotListenerEnabled: true });
-/* const firebasemock = require('firebase-mock');
 
-const mockauth = new firebasemock.MockFirebase();
-const mockfirestore = new firebasemock.MockFirestore();
-mockfirestore.autoFlush();
-mockauth.autoFlush();
-
-global.firebase = firebasemock.MockFirebaseSdk(
-  // use null if your code does not use RTDB
-  () => null,
-  () => mockauth,
-  () => mockfirestore,
-); */
 // Función userData
 describe('función userData', () => {
   it('Deberia ser una función', () => {
     expect(typeof userData).toBe('function');
   });
-  it('Debería poder iniciar sesion', () => signIn('kelmita22@gmail.com', '123456')
-    .then((user) => {
-      expect(user.email).toBe('kelmita22@gmail.com');
-    }));
-});
-// Función getUserData
-describe('función getUserData', () => {
-  it('Deberia ser una función', () => {
-    expect(typeof getUserData).toBe('function');
+  it('Deberia crear un nuevo usuario', () => {
+    const currentUser = {
+      uid: 'uid_001',
+      displayName: 'Kelly',
+      photoURL: 'prueba.jpg',
+      email: 'kelmita22@gmail.com',
+    };
+    const current = {
+      uid: 'uid_002',
+      displayName: null,
+      photoURL: null,
+      email: 'kelmita22@gmail.com',
+    };
+    userData(currentUser)
+      .then(() => {
+        getUserData('uid_001')
+          .then((doc) => {
+            expect(doc.data().email).toEqual('kelmita22@gmail.com');
+            expect(doc.data().username).toEqual('Kelly');
+            expect(doc.data().photo).toEqual('prueba.jpg');
+          });
+      });
+    userData(current)
+      .then(() => {
+        getUserData('uid_002')
+          .then((doc) => {
+            expect(doc.data().username).toEqual('User');
+            expect(doc.data().photo).toEqual('./imageProject/avatar.png');
+          });
+      });
   });
-  it('Debería poder iniciar sesion', () => signIn('kelmita22@gmail.com', '123456')
-    .then((user) => {
-      expect(user.email).toBe('kelmita22@gmail.com');
-    }));
 });
+
 // Función updateCurrentUser
 describe('función updateCurrentUser', () => {
   it('Deberia ser una función', () => {
     expect(typeof updateCurrentUser).toBe('function');
   });
-  it('Debería poder iniciar sesion', () => signIn('kelmita22@gmail.com', '123456')
-    .then((user) => {
-      expect(user.email).toBe('kelmita22@gmail.com');
+  it('Deberia actualizar los datos del usuario', (done) => updateCurrentUser('uid_001', 'Kelly', '', '', '', '')
+    .then(() => {
+      getUserData('uid_001')
+        .then((doc) => {
+          expect(doc.data().username).toBe('Kelly');
+          done();
+        });
     }));
 });
 // Función postAdd
-describe('función postAdd', () => {
+describe('Función postAdd', () => {
   it('Deberia ser una función', () => {
     expect(typeof postAdd).toBe('function');
   });
-  it('Debería poder iniciar sesion', () => signIn('kelmita22@gmail.com', '123456')
-    .then((user) => {
-      expect(user.email).toBe('kelmita22@gmail.com');
-    }));
-});
-// Función getPost
-describe('función getPost', () => {
-  it('Deberia ser una función', () => {
-    expect(typeof getPost).toBe('function');
-  });
-  it('Debería poder iniciar sesion', () => signIn('kelmita22@gmail.com', '123456')
-    .then((user) => {
-      expect(user.email).toBe('kelmita22@gmail.com');
-    }));
+  it('Deberia crear un post', (done) => postAdd('uid_001', 'Kelly', '', '', '')
+    .then(() => getPost(
+      (data) => {
+        const result = data.find((post) => post.publication === 'Nueva publicación');
+        expect(result.publication).toBe('Nueva publicación');
+        done();
+      },
+    )));
 });
 // Función commentAdd
-describe('función commentAdd', () => {
+describe('Función commentAdd', () => {
   it('Deberia ser una función', () => {
     expect(typeof commentAdd).toBe('function');
   });
-  it('Debería poder iniciar sesion', () => signIn('kelmita22@gmail.com', '123456')
-    .then((user) => {
-      expect(user.email).toBe('kelmita22@gmail.com');
-    }));
-});
-// Función getComment
-describe('función getComment', () => {
-  it('Deberia ser una función', () => {
-    expect(typeof getComment).toBe('function');
-  });
-  it('Debería poder iniciar sesion', () => signIn('kelmita22@gmail.com', '123456')
-    .then((user) => {
-      expect(user.email).toBe('kelmita22@gmail.com');
-    }));
-});
-// Función removePost
-describe('función removePost', () => {
-  it('Deberia ser una función', () => {
-    expect(typeof removePost).toBe('function');
-  });
-  it('Debería poder iniciar sesion', () => signIn('kelmita22@gmail.com', '123456')
-    .then((user) => {
-      expect(user.email).toBe('kelmita22@gmail.com');
-    }));
-});
-// Función removeComment
-describe('función removeComment', () => {
-  it('Deberia ser una función', () => {
-    expect(typeof removeComment).toBe('function');
-  });
-  it('Debería poder iniciar sesion', () => signIn('kelmita22@gmail.com', '123456')
-    .then((user) => {
-      expect(user.email).toBe('kelmita22@gmail.com');
-    }));
-});
-// Función profilePhoto
-describe('función profilePhoto', () => {
-  it('Deberia ser una función', () => {
-    expect(typeof profilePhoto).toBe('function');
-  });
-  it('Debería poder iniciar sesion', () => signIn('kelmita22@gmail.com', '123456')
-    .then((user) => {
-      expect(user.email).toBe('kelmita22@gmail.com');
-    }));
-});
-// Función coveragePhoto
-describe('función coveragePhoto', () => {
-  it('Deberia ser una función', () => {
-    expect(typeof coveragePhoto).toBe('function');
-  });
-  it('Debería poder iniciar sesion', () => signIn('kelmita22@gmail.com', '123456')
-    .then((user) => {
-      expect(user.email).toBe('kelmita22@gmail.com');
-    }));
+  it('Deberia agregar una nuevo comentario', (done) => commentAdd('uid_001', 'id_001', 'Hola')
+    .then(() => getComment('id_001',
+      (data) => {
+        const result = data.find((comment) => comment.comment === 'Hola');
+        expect(result.comment).toBe('Hola');
+        done();
+      })));
 });
 // Función upgradePost
-describe('función upgradePost', () => {
+describe('Función upgradePost', () => {
   it('Deberia ser una función', () => {
     expect(typeof upgradePost).toBe('function');
   });
-  it('Debería poder iniciar sesion', () => signIn('kelmita22@gmail.com', '123456')
-    .then((user) => {
-      expect(user.email).toBe('kelmita22@gmail.com');
-    }));
-});
-// Función upgradePost
-describe('función upgradeComment', () => {
-  it('Deberia ser una función', () => {
-    expect(typeof upgradeComment).toBe('function');
-  });
-  it('Debería poder iniciar sesion', () => signIn('kelmita22@gmail.com', '123456')
-    .then((user) => {
-      expect(user.email).toBe('kelmita22@gmail.com');
-    }));
-});
-// Función upgradeLike
-describe('función upgradeLike', () => {
-  it('Deberia ser una función', () => {
-    expect(typeof upgradeLike).toBe('function');
-  });
-  it('Debería poder iniciar sesion', () => signIn('kelmita22@gmail.com', '123456')
-    .then((user) => {
-      expect(user.email).toBe('kelmita22@gmail.com');
-    }));
+  it('Deberia actualizar información del post', (done) => upgradePost('id_001', 'Hola chicos')
+    .then(() => getPost(
+      (data) => {
+        const result = data.find((post) => post.publication === 'Hola chicos');
+        expect(result.publication).toBe('Hola chicos');
+        done();
+      },
+    )));
 });
